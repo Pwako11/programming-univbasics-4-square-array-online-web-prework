a83cc281f284fa0159c6fbd37c1dd1a299448604
Patrick A <akolo@hotmail.com>
2020-01-19 17:43:08 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,22 +1,12 @@
+# require 'byebug'
 def square_array(array)
-  binding.pry
-  # array = numbers
-  # counter = 0
-  # square_array1 = []
+  # byebug
+  counter = 0
+  new_array = []
   
-  # while counter < array.length do 
-  #   square_array1<< array[counter]**2
-  #   counter += 1 
-  # end 
-  
-  # array = [9,10,16,25]
-  # counter = 0
-  # square_array2 = []
-  
-  # while counter < array.length do 
-  #   square_array2<< array[counter]**2
-  #   counter += 1 
-  # end 
-  # square_array1
-  # square_array2
+  while counter < array.length do 
+    new_array<< array[counter]**2
+    counter += 1 
+  end 
+  new_array
 end
