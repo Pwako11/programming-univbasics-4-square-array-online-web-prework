2d86840d29870e1541f327aef24c99a8cdd5245a
Patrick A <akolo@hotmail.com>
2020-01-19 17:42:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,12 +2,11 @@ require 'byebug'
 def square_array(array)
   byebug
   counter = 0
-  square_array = []
+  new_array = []
   
   while counter < array.length do 
-    square_array2<< array[counter]**2
+    new_array<< array[counter]**2
     counter += 1 
   end 
-  square_array1
-  square_array2
+  new_array
 end
