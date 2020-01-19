6f6832b60505ee938e6c823571af0c8d0f515811
Patrick A <akolo@hotmail.com>
2020-01-19 17:29:32 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,22 @@
 def square_array(array)
-  # your code here
+  binding.pry
+  # array = numbers
+  # counter = 0
+  # square_array1 = []
+  
+  # while counter < array.length do 
+  #   square_array1<< array[counter]**2
+  #   counter += 1 
+  # end 
+  
+  # array = [9,10,16,25]
+  # counter = 0
+  # square_array2 = []
+  
+  # while counter < array.length do 
+  #   square_array2<< array[counter]**2
+  #   counter += 1 
+  # end 
+  # square_array1
+  # square_array2
 end
