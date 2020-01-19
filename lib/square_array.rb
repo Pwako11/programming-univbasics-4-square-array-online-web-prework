06746d516e595aeec3d884ff4c90d43c7ce14b75
Patrick A <akolo@hotmail.com>
2020-01-19 17:41:19 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,23 +1,13 @@
 require 'byebug'
 def square_array(array)
   byebug
-  # array = numbers
-  # counter = 0
-  # square_array1 = []
+  counter = 0
+  square_array = []
   
-  # while counter < array.length do 
-  #   square_array1<< array[counter]**2
-  #   counter += 1 
-  # end 
-  
-  # array = [9,10,16,25]
-  # counter = 0
-  # square_array2 = []
-  
-  # while counter < array.length do 
-  #   square_array2<< array[counter]**2
-  #   counter += 1 
-  # end 
-  # square_array1
-  # square_array2
+  while counter < array.length do 
+    square_array2<< array[counter]**2
+    counter += 1 
+  end 
+  square_array1
+  square_array2
 end
